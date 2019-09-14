66fb9c91d975fd10790816082aed9e67ec55c308
GQSM <ms314006@hotmail.com>
2019-09-14 21:50:13 +0800

    [ADD] 增加預設的筆記圖片資料

---
+++ src/assets/noteBackground.ts
@@ -0,0 +1,9 @@
+const noteBackgroundImages = [
+  'andrej-lisakov-V2OyJtFqEtY-unsplash',
+  'annie-spratt-0ZPSX_mQ3xI-unsplash',
+  'bharath-g-s-aLGiPJ4XRO4-unsplash',
+  'ivan-gromov-Y3vPEuNlf7w-unsplash',
+  'pawel-czerwinski-dgJT71cXlC4-unsplash',
+];
+
+export default noteBackgroundImages;
